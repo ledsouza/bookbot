c82d8d212b9f9a6866c8f0ca2ae39f9220632ef8
ledsouza <leandro.souza.159@example.com>
2024-03-29 20:34:37 -0300

    finisehd bootdev projetc

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
+
     n_words = get_number_of_words(text)
-    print(f"Number of words in the book: {n_words}")
+    letter_count = count_letters(text)
+    report(n_words, letter_count)
 
 def get_book_text(path):
     with open(path) as f:
@@ -11,6 +13,22 @@ def get_book_text(path):
 def get_number_of_words(text):
     words = text.split()
     return len(words)
+
+def count_letters(text):
+    letter_count = {}
+    lower_case_text = text.lower()
+    for letter in lower_case_text:
+        if letter.isalpha():
+            letter_count[letter] = letter_count.get(letter, 0) + 1
+    return letter_count
+
+def report(n_words, letter_count):
+    print('--- Begin report of books/frankenstein.txt ---')
+    print(f'{n_words} words found in the document\n')
+    sorted_letter_count = sorted(letter_count.items(), key=lambda x: x[1], reverse=True)
+    for letter, count in sorted_letter_count:
+        print(f'The "{letter}" character was found {count} times')
+    print('--- End report ---')
 
 if __name__ == '__main__':
     main()
